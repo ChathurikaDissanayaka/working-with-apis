5d92e47b4acbfae1c4cf0a5adab3ab66bc30dd85
Sachini <sachichathudissanayaka@gmail.com>
2024-10-01 20:14:21 +0530

    Add changes

---
+++ Intro to apis and boredbot/dog-api-fetch-and-dom-practice/index.js
@@ -0,0 +1,31 @@
+/**
+Challenge:
+
+1. Fetch a random image from the Dog API again 
+(https://dog.ceo/api/breeds/image/random)
+
+2. Access the DOM and insert the URL you got from the
+API as an image `src` property (probably easiest if 
+you create the image completely here in the JS and add 
+it as the innerHTML of another element on the DOM)
+*/
+
+async function getData() {
+    const url = "https://dog.ceo/api/breeds/image/random";
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+  
+      const json = await response.json();
+      // console.log(json.message);
+      document.getElementById("img").innerHTML += `<img src=${json.message}>`
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+  
+  getData()
+  
+  
