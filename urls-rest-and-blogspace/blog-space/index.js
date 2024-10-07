44901886642221f28976f0eefda28cbf7c84cb5c
Sachini <sachichathudissanayaka@gmail.com>
2024-10-07 19:15:16 +0530

    Add changes

--- urls-rest-and-blogspace/blog-space/index.js
+++ urls-rest-and-blogspace/blog-space/index.js
@@ -3,4 +3,10 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts")
     .then(data => {
         const postsArr = data.slice(0, 5)
         console.log(postsArr)
+        const postsEl = document.getElementById('posts')
+        postsArr.forEach((item) => {
+          postsEl.innerHTML += `
+          <h1>${item.title}</h1>
+          <p>${item.body}</p>`
+        })
     })
