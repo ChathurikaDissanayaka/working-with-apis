8e99937731f25a4466e6c5306b781765f3514a4b
Sachini <sachichathudissanayaka@gmail.com>
2024-10-19 22:23:36 +0530

    Add changes

--- promise-rejection-and-capstone/index.js
+++ promise-rejection-and-capstone/index.js
@@ -4,43 +4,39 @@ const timeEl = document.getElementById('time')
 const weather = document.getElementById('weather')
 
 // Display background image
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
-    .then(res => {
-        if(!res.ok){
-            throw Error("Something went wrong")
-        }
-        return res.json()
-    })
-    .then(data => {
-        document.body.style.backgroundImage = `url(${data.urls.full})`
-		document.getElementById("author").textContent = `By: ${data.user.name}`
-    })
-	.catch(err => {
-        const url = 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI2NzA5Mzg&ixlib=rb-1.2.1&q=85'
-        document.body.style.backgroundImage = `url(${url})`
-        document.getElementById("author").textContent = 'By: Sean O.'
-        console.error(err)
-    })
+try {
+    const resBackground = await fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+    if(!resBackground.ok){
+        throw Error("Something went wrong")
+    }
+    const dataBackground = await resBackground.json()
+    document.body.style.backgroundImage = `url(${dataBackground.urls.full})`
+    document.getElementById("author").textContent = `By: ${dataBackground.user.name}`
+} catch(err) {
+    const url = 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI2NzA5Mzg&ixlib=rb-1.2.1&q=85'
+    document.body.style.backgroundImage = `url(${url})`
+    document.getElementById("author").textContent = 'By: Sean O.'
+    console.error(err)
+}
 
 // Display crypto data
-fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
-    .then(res => {
-        if (!res.ok) {
-            throw Error("Crypto data not available")
-        }
-        return res.json()
-    })
-    .then(data => {
-		cryptoTop.innerHTML = `
-			<img src=${data.image.small} alt="">
-            <p>${data.id}</p>`
+try {
+    const resCrypto = await fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+    if (!resCrypto.ok) {
+        throw Error("Crypto data not available")
+    }
+    const dataCrypto = await resCrypto.json()
+    cryptoTop.innerHTML = `
+        <img src=${dataCrypto.image.small} alt="">
+        <p>${dataCrypto.id}</p>`
 
-		cryptoBottom.innerHTML = `
-				<p><i class="fa-solid fa-coins" style="color: #FFD43B;"></i> $${data.market_data.current_price.usd}</p>
-                <p><i class="fa-solid fa-up-long" style="color: #63E6BE;"></i> $${data.market_data.high_24h.usd}</p>
-                <p><i class="fa-solid fa-down-long" style="color: #fb6060;"></i> $${data.market_data.low_24h.usd}</p>`
-    })
-    .catch(err => console.error(err))
+    cryptoBottom.innerHTML = `
+        <p><i class="fa-solid fa-coins" style="color: #FFD43B;"></i> $${dataCrypto.market_data.current_price.usd}</p>
+        <p><i class="fa-solid fa-up-long" style="color: #63E6BE;"></i> $${dataCrypto.market_data.high_24h.usd}</p>
+        <p><i class="fa-solid fa-down-long" style="color: #fb6060;"></i> $${dataCrypto.market_data.low_24h.usd}</p>`
+} catch(err) {
+    console.error(err)
+}
 
 // Display time
 setInterval(() => {
@@ -49,22 +45,20 @@ setInterval(() => {
 }, 1000);
 
 // Display weather data
-navigator.geolocation.getCurrentPosition((position) => {
-    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`)
-        .then(res => {
-            if(!res.ok){
-                throw Error("Weather data not available")
-            }
-            return res.json()
-        })
-        .then(data => {
-            console.log(data)
-            const iconUrl = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
-            const temp = Math.round(data.main.temp)
-            weather.innerHTML = `
-                <img src=${iconUrl} class="weather-icon">
-                <p class="temp">${temp}º</p>
-                <p class="city">${data.name}</p>`
-        })
-        .catch(err => console.error(err))
+navigator.geolocation.getCurrentPosition(async position => {
+    try {
+        const resWeather = await fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`)
+        if (!resWeather.ok){
+            throw Error("Weather data not available")
+        }
+        const dataWeather =  await resWeather.json()
+        const iconUrl = `https://openweathermap.org/img/wn/${dataWeather.weather[0].icon}@2x.png`
+        const temp = Math.round(dataWeather.main.temp)
+        weather.innerHTML = `
+            <img src=${iconUrl} class="weather-icon">
+            <p class="temp">${temp}º</p>
+            <p class="city">${dataWeather.name}</p>`
+    } catch(err) {
+        console.error(err)
+    }  
 })
